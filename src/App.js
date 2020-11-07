138ec5db6a029efef9d4c7b2193074e91eb5bf8d
Dean Tornado <deantornadoo@gmail.com>
2020-11-07 19:40:51 +0700

    Test Branch

--- src/App.js
+++ src/App.js
@@ -3,11 +3,6 @@ import React from "react";
 import "./App.css";
 
 class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { name: "Dean Tornado" };
-    this.state.a = "Yo what up!";
-  }
   render() {
     return (
       <div>
