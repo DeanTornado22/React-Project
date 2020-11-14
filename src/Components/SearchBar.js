5c11367cfc1e44c607a4c07c6f029f63fbc755c3
DeanTornado22 <deantornadoo@gmail.com>
2020-11-15 04:54:36 +0700

    Emoji Search Updated

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,15 +1,42 @@
 import React from "react";
 import {} from "../assets/css/searchbar.css";
 import firebase from "firebase/app";
-import "firebase/analytics";
 import "firebase/auth";
 import "firebase/firestore";
 import { firebaseConfig } from "../config/firebase.config";
-firebase.initializeApp(firebaseConfig);
+
+try {
+  firebase.initializeApp(firebaseConfig);
+} catch (e) {
+  console.log(e.message);
+}
+const db = firebase.firestore();
 
 export class SearchBar extends React.Component {
   constructor(props) {
     super(props);
+  }
+
+  componentDidMount() {
+    // db.collection("users")
+    //   .add({
+    //     first: "Ada",
+    //     last: "Lovelace",
+    //     born: 1815,
+    //   })
+    //   .then(function (docRef) {
+    //     console.log("Document written with ID: ", docRef.id);
+    //   })
+    //   .catch(function (error) {
+    //     console.error("Error adding document: ", error);
+    //   });
+    db.collection("users")
+      .get()
+      .then((querySnapshot) => {
+        querySnapshot.forEach((doc) => {
+          console.log(`${doc.id} => ${JSON.stringify(doc.data())}`);
+        });
+      });
   }
   render() {
     return (
