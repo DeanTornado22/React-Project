252c82d89901da88edaefbf85331f2f394f70588
DeanTornado22 <deantornadoo@gmail.com>
2020-11-14 20:59:08 +0700

    Emoji Search

---
+++ src/config/firebase.config.js
@@ -0,0 +1,10 @@
+export const firebaseConfig = {
+  apiKey: "AIzaSyC72To4VCTo4jUdu-duMBtoXcTN4aJ2kjE",
+  authDomain: "dbp-ci.firebaseapp.com",
+  databaseURL: "https://dbp-ci.firebaseio.com",
+  projectId: "dbp-ci",
+  storageBucket: "dbp-ci.appspot.com",
+  messagingSenderId: "218851100033",
+  appId: "1:218851100033:web:bca74ecf5b81277ff95480",
+  measurementId: "G-TTYCW4HBR3",
+};
