f137ac1fec4035dfd6eab01adaf6bf667182a7e9
DeanTornado22 <deantornadoo@gmail.com>
2020-11-14 21:01:19 +0700

    Edit SearchBar.js

--- src/Components/SearchBar.js
+++ src/Components/SearchBar.js
@@ -1,5 +1,11 @@
 import React from "react";
 import {} from "../assets/css/searchbar.css";
+import firebase from "firebase/app";
+import "firebase/analytics";
+import "firebase/auth";
+import "firebase/firestore";
+import { firebaseConfig } from "../config/firebase.config";
+firebase.initializeApp(firebaseConfig);
 
 export class SearchBar extends React.Component {
   constructor(props) {
