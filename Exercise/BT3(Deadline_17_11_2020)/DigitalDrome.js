24ade594b4d5b0ff66d5b3b85dd029cf4cd97bb3
DeanTornado22 <deantornadoo@gmail.com>
2020-11-17 23:09:05 +0700

    Update DigitalDrome

--- Exercise/BT3(Deadline_17_11_2020)/DigitalDrome.js
+++ Exercise/BT3(Deadline_17_11_2020)/DigitalDrome.js
@@ -1,21 +1,52 @@
-function digitaldrome(numberInput) {}
-var stringNumber = numberInput.toString();
-var b = a.split(",").map(function (item) {
-  return parseInt(item, 10);
+Object.defineProperties(Array.prototype, {
+  count: {
+    value: function (value) {
+      return this.filter((x) => x == value).length;
+    },
+  },
 });
-var isAscending = (a) =>
-  a
-    .slice(1)
-    .map((e, i) => e > a[i])
-    .every((x) => x);
-var isDescending = (a) =>
-  a
-    .slice(1)
-    .map((e, i) => e < a[i])
-    .every((x) => x);
 
-console.log(isAscending([1, 2, 3, 4]));
-console.log(isAscending([1, 2, 5, 4]));
-console.log(isAscending([1, 2, 5, 4]));
-console.log(isDescending([6, 2, 1, -1]));
-console.log(b);
+function digitaldrome(numberInput) {
+  var stringNumber = numberInput.toString();
+  var numberArray = stringNumber.split("").map(Number);
+  var isAscending = (a) =>
+    a
+      .slice(1)
+      .map((e, i) => e >= a[i])
+      .every((x) => x);
+  var isDescending = (a) =>
+    a
+      .slice(1)
+      .map((e, i) => e <= a[i])
+      .every((x) => x);
+
+  var firstNum = numberArray[0];
+  if (numberArray.count(firstNum) == numberArray.length) {
+    return "Repdrome";
+  } else {
+    if (isAscending(numberArray) == true) {
+      for (let i = 0; i < numberArray.length; i++) {
+        if (numberArray.indexOf(numberArray[i], i) != -1) {
+          return "Plaindrome";
+        }
+      }
+      return "Metadrome";
+    } else if (isDescending(numberArray) == true) {
+      for (let i = 0; i < numberArray.length; i++) {
+        if (numberArray.indexOf(numberArray[i], i + 1) != -1) {
+          return "Nialpdrome";
+        }
+      }
+      return "katadrome";
+    } else {
+      return "Nondrome";
+    }
+  }
+}
+
+console.log(digitaldrome(484655));
+console.log(digitaldrome(666));
+console.log(digitaldrome(1357));
+console.log(digitaldrome(13577));
+console.log(digitaldrome(765));
+console.log(digitaldrome(7655));
