ed6b432896751ab9294b3bb1911b57a847ea8ed3
DeanTornado22 <deantornadoo@gmail.com>
2020-11-21 20:42:39 +0700

    Update Master

--- src/App.js
+++ src/App.js
@@ -1,25 +1,7 @@
 import logo from "./logo.svg";
 import React from "react";
 import "./App.css";
-import { Header } from "./Components/Header.js";
-import { SearchBar } from "./Components/SearchBar.js";
-import { Emoji } from "./Components/Emoji.js";
-import { emojiList } from "./Sources/emojiList.js";
 
-function App() {
-  let eList = [];
-  for (let i = 0; i < 20; i++) {
-    eList.push(
-      <Emoji title={emojiList[i].title} symbol={emojiList[i].symbol} />
-    );
-  }
-  return (
-    <React.Fragment>
-      <Header />
-      <SearchBar />
-      {eList}
-    </React.Fragment>
-  );
-}
+function App() {}
 
 export default App;
